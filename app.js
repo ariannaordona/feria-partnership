f8b66ed28fa92380dca7c501172eeee338e24ae3
Arianna Ordona <ariannaordona@gmail.com>
2019-04-06 20:59:46 +0000

    added media queries for font resizing on different devices

--- app.js
+++ app.js
@@ -6,6 +6,17 @@ var express         = require('express'),
 app.set('view engine', 'ejs');
 app.use(serveStatic('FERIA/'));
 
+const sgMail = require('@sendgrid/mail');
+sgMail.setApiKey(process.env.SENDGRID_API_KEY);
+const msg = {
+  to: 'ariannaordona@gmail.com',
+  from: 'test@example.com',
+  subject: 'Sending with SendGrid is Fun',
+  text: 'and easy to do anywhere, even with Node.js',
+  html: '<strong>and easy to do anywhere, even with Node.js</strong>',
+};
+sgMail.send(msg);
+
 //====================================
 // ROUTES
 //====================================
